38ac1078968db853db1de8074ee4645230add5f9
Phongpak Taothong <phongpak@bsd.education>
2020-01-24 14:13:53 +0700

    Preparing front end for collaboration work

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FemtoGooDaKi
+
+folders are according to package
+
+1.Frontend
---
+++ frontend/src/page/LoginPage.jsx
@@ -0,0 +1,11 @@
+//Yoss
+import React from 'react';
+
+export class LoginPage extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
