d914592c1b73e9776525a250db21ef6740a8c89c
SSripilaipong <vappakarat@gmail.com>
2020-01-31 18:22:33 +0700

    refactor user detail endpoint

---
+++ backend/user/detail.py
@@ -0,0 +1,12 @@
+def get_user_detail(user_id):
+    result = {
+        'id': user_id,
+        'firstName': 'Chain',
+        'lastName': 'Sripilaipong',
+        'birthDate': '1998-07-25',
+        'age': 21,
+        'registerDate': '2020-01-01',
+        'job': 'Data Engineer',
+    }
+
+    return result
--- backend/user/main.py
+++ backend/user/main.py
@@ -3,32 +3,18 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
-from .util import user_detail_fields
-
 app = Flask(__name__)
 
 
-def _get_user_detail(user_id):
-    result = {
-        'id': user_id,
-        'firstName': 'Chain',
-        'lastName': 'Sripilaipong',
-        'birthDate': '1998-07-25',
-        'age': 21,
-        'registerDate': '2020-01-01',
-        'job': 'Data Engineer',
-    }
-
-    return result
-
-
 @app.route('/detail', methods=['POST'])
-def get_user_detail():
+def user_detail():
+    from .detail import get_user_detail
+
     user_id = request.form.get('userId', None)
     if user_id is None:
         response = flask.Response('please provide userId', status=400)
     else:
-        result = _get_user_detail(user_id)
+        result = get_user_detail(user_id)
         response = jsonify(result)
 
     return response
