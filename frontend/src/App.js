bc525b4d3c8d02080d19f9f54a121e6ac5830242
Akamate <aukmate@gmail.com>
2020-01-31 09:31:35 +0700

    Add Login Page and Css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import '../src/page/LoginPage'
+import { LoginPage } from '../src/page/LoginPage';
 function App() {
   return (
-    <div className="App" id="app"/>
+   // <div className="App" id="app"/>
+    <LoginPage/>
   );
 }
 
