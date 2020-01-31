787387eb77a68f21cf678cf361f127d11b0ddfbe
SSripilaipong <vappakarat@gmail.com>
2020-01-31 19:10:29 +0700

    add login and token endpoints

--- backend/user/main.py
+++ backend/user/main.py
@@ -29,5 +29,41 @@ def register_user():
     return response
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    from .login import login
+
+    username = request.form.get('username', None)
+    password = request.form.get('password', None)
+
+    if username is None:
+        response = flask.Response('please provide username', status=400)
+    elif password is None:
+        response = flask.Response('please provide password', status=400)
+    else:
+        result, status_code = login(username, password)
+        response = jsonify(result), status_code
+
+    return response
+
+
+@app.route('/validateToken', methods=['POST'])
+def validate_token():
+    from .login import validate_token
+
+    username = request.form.get('username', None)
+    token = request.form.get('token', None)
+
+    if username is None:
+        response = flask.Response('please provide username', status=400)
+    elif token is None:
+        response = flask.Response('no token to validate', status=400)
+    else:
+        result, status_code = validate_token(username, token)
+        response = jsonify(result), status_code
+
+    return response
+
+
 if __name__ == '__main__':
     app.run()
--- backend/user/util.py
+++ backend/user/util.py
@@ -1,8 +1,12 @@
 user_detail_fields = [
     'id',
+    'username',
+    'password',
     'firstName',
     'lastName',
     'birthDate',
     'registerDate',
     'job',
 ]
+
+SALT = b'\xfb\xcd=r\xaf[\xaf\xbc\xbdk\x8e$\xa2\x9eG\xb7\xd4P\xe68b\xbc\xde\xcb\xd0\x99@0>Hb\xa9'
