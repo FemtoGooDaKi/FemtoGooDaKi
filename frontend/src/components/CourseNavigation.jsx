191a02d528dc16ff745fbe5159da2672ac4a43bc
Kevsg <31314322+Kevsg@users.noreply.github.com>
2020-02-08 18:48:39 +0700

    Add CourseNavigation (#7)
    Co-authored-by: Natchanon A. <natchanon.347@gmail.com>

---
+++ frontend/src/components/CourseNavigation.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './CourseNavigation.css';
+
+/*
+interface ICourseNavigationProps {
+	name: string;
+	subject: [Subject]
+	onClick = (knowledgeName) => any
+}
+*/
+
+export default class CourseNavigation extends Component {
+    render() {
+        return (
+            <div className='coursenav-container'>
+                <div className='coursenav-name'>{this.props.name}</div>
+                <div className='coursenav-separator'></div>
+                {this.props.subject.map((subject) => 
+                    <CourseNavigationSubject
+                    subject={subject}
+                    handleKnowledgeClick={this.props.onClick}
+                    />
+                )}
+            </div>
+        );
+    }
+}
+
+class CourseNavigationSubject extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showKnowledge: false,
+        };
+        console.log(props.subject.knowledge)
+    }
+
+    toggleKnowledge = () => {
+        this.setState({
+            showKnowledge: !this.state.showKnowledge,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='coursenav-subject-container'>
+                    <div
+                    className='coursenav-subject-name'
+                    onClick={this.toggleKnowledge}
+                    >
+                        {this.props.subject.name}
+                    </div>
+                    {this.state.showKnowledge &&
+                        this.props.subject.knowledge.map((knowledge) => 
+                            <div className='coursenav-subject-knowledge'
+                            onClick={() => { this.props.handleKnowledgeClick(knowledge.name) }}
+                            >
+                                {knowledge.name}
+                            </div>
+                        )
+                    }
+                </div>
+                <div className='coursenav-separator'></div>
+            </div>
+        );
+    }
+}
