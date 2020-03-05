f9543570db1cdcee121916bc3e365dcd74116c69
Akamate <aukmate@gmail.com>
2020-03-05 19:30:56 +0700

    integration loginpage

--- frontend/src/page/LoginPage/LoginPage.jsx
+++ frontend/src/page/LoginPage/LoginPage.jsx
@@ -2,13 +2,17 @@
 import React from "react";
 import "./LoginPage.css";
 import logo from "./Logo.svg";
+import { connect } from "react-redux";
+import { setLoginFlag } from "../../actions";
+import { Redirect } from 'react-router-dom'
 
-export class LoginPage extends React.Component {
+class LoginPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
-      password: ""
+      password: "",
+      redirect: null
     };
   }
 
@@ -20,35 +24,55 @@ export class LoginPage extends React.Component {
     alert("Pressed");
   };
 
+  handleLoginButton = () => {
+     setLoginFlag(true);
+     this.setState({redirect : '/mycourse'})
+  }
+
   render() {
-    return (
-      <div className="login-page-container">
-        <div>
-          <img src={logo} alt="" />
-        </div>
-        <div>
-          <h1 className="header">Log In to your account!</h1>
-          <input
-            name="username"
-            placeholder="Username"
-            onChange={e => this.handleInputChange(e)}
-          />
-          <input
-            name="password"
-            placeholder="Password"
-            onChange={e => this.handleInputChange(e)}
-            type="password"
-          />
+    if (this.state.redirect) {
+       return <Redirect to={this.state.redirect} />
+    }
+    else {
+      return (
+        <div className="login-page-container">
           <div>
-            <button className="backButton" onClick={this.handleBackButton}>
-              BACK
-            </button>
-            <button className="loginButton" onClick={this.handleLoginButton}>
-              LOG IN
-            </button>
+            <img src={logo} alt="" />
+          </div>
+          <div>
+            <h1 className="header">Log In to your account!</h1>
+            <input
+              name="username"
+              placeholder="Username"
+              onChange={e => this.handleInputChange(e)}
+            />
+            <input
+              name="password"
+              placeholder="Password"
+              onChange={e => this.handleInputChange(e)}
+              type="password"
+            />
+            <div>
+              <button className="backButton" onClick={this.handleBackButton}>
+                BACK
+              </button>
+              <button className="loginButton" onClick={this.handleLoginButton}>
+                LOG IN
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
+
+const mapStateToProps = state => ({
+  login: state.login
+});
+
+const mapDispatchToProps = dispatch => ({
+  setLoginFlag: (flag) => dispatch(setLoginFlag(flag))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
