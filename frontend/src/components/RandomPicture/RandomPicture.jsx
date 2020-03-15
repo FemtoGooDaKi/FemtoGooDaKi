720884d5e417fa72ad865b290fc3d52c10422385
Kevsg <31314322+Kevsg@users.noreply.github.com>
2020-03-15 10:54:05 +0700

    Add RandomImage (#17)
    Co-authored-by: Natchanon <natchanon.347@gmail.com>

---
+++ frontend/src/components/RandomPicture/RandomPicture.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+/*
+Props
+- className: String
+*/
+
+export default class RandomImage extends Component {
+    imageUrls = [
+        'https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/261909/pexels-photo-261909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/159823/kids-girl-pencil-drawing-159823.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/267491/pexels-photo-267491.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/1181675/pexels-photo-1181675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/374820/pexels-photo-374820.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/1741230/pexels-photo-1741230.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/1089440/pexels-photo-1089440.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    ];
+
+    randomUrl = () => {
+        return this.imageUrls[Math.floor(Math.random() * (this.imageUrls.length + 1))];
+    }
+
+    render() {
+        return (
+            <img
+            className={this.props.className}
+            src={ this.randomUrl() }
+            style={{ width: '100%', height: '100%' }}
+            />
+        );
+    }
+}
