a02c26a17e80405b05365250c6c6d4bc91df5019
SSripilaipong <vappakarat@gmail.com>
2020-04-06 22:00:21 +0700

    change SECRET_KEY

--- backend/course/course/course/settings.py
+++ backend/course/course/course/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tffept$n=5n15$pb-&@b)vy9ivdumzcmnczvf-_uj6_)hp21i$'
+SECRET_KEY = 'pe)s1_l*d=o51tzzq_vfqf5%op*+fk)&26h8^4nvg*65xhg36b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
