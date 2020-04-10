1f2e4a20040d6937b4cb5710b4f7941804967c81
SSripilaipong <vappakarat@gmail.com>
2020-04-11 00:39:36 +0700

    add endpoints for create and search course

--- backend/course/course/main_course/models.py
+++ backend/course/course/main_course/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from main_knowledge.models import Knowledge
+
 
 # Create your models here.
+class Course(models.Model):
+    author = models.CharField(max_length=30)
+    courseName = models.CharField(max_length=128)
+    description = models.TextField()
+    knowledge_set = models.ManyToManyField(Knowledge)
+    createDate = models.DateField()
+    job = models.CharField(max_length=30)
