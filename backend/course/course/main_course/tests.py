568a5cacbacc42e7cec3f68a4ab3dfd6ad5b11c4
Natthapong Jiemjintanarom <natthapong41@gmail.com>
2020-04-11 13:20:18 +0700

    Add Knowledge test

--- backend/course/course/main_course/tests.py
+++ backend/course/course/main_course/tests.py
@@ -1,3 +1,75 @@
 from django.test import TestCase
+from main_course.models import Course
+from django.http import HttpRequest
+from main_course.views import course_endpoint, course_id_endpoint, create_course, search_course
+from datetime import datetime
+import json
+from io import BytesIO
+from main_knowledge.models import Knowledge
 
-# Create your tests here.
+class courseTestCase(TestCase):
+
+    def setUp(self):
+        self.knowledge = Knowledge.objects.create(
+            author="author001",
+            subject="subject001",
+            content="hihihihihihihihihihhihi",
+            createDate=datetime(2020,3,1)
+        )
+        # course = Course.objects.create(
+        #     author="author001",
+        #     courseName="course001",
+        #     description="description",
+        #     createDate=datetime(2020,3,4),
+        #     job="Author"
+        # )
+        self.course2 = Course(
+            id="2",
+            author="author002",
+            courseName="course002",
+            description="description",
+            createDate=datetime(2020,2,4),
+            job="Author"
+        ).save()
+        course2_ = Course.objects.get(id="2")
+        self.knowledge_set2 = course2_.knowledge_set.create(
+            author="author002",
+            subject="subject002",
+            content="hihihihihihihihihihhihi",
+            createDate=datetime(2020,2,1)
+        ).save
+
+    def mock_request(self, method, content = json.dumps({}), header = {"Content-Type":"application/json"}):
+        request1 = HttpRequest()
+        request1.method = method
+        body_content = content
+        request1._stream = BytesIO(body_content.encode())
+        request1.content_type = "application/json"
+        request1.headers = header
+
+        return request1
+    
+    # ------------------------------------
+
+    def test_model(self):
+        course = Course.objects.get(author="author002")
+        self.assertEquals(course.courseName,"course002")
+    
+    # ------------------------------------
+
+    def test_course_endpoint_wrong_method(self):
+        authen = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNoYWluIn0.Zrm7f9vw9qLA8dWxK9TRDZxO7U1XZQqSsgxFitf8Leg'
+        headers = {"Content-Type":"application/json","Authorization":str(authen)}
+        endpoint_response = course_endpoint(courseTestCase.mock_request(self,"VIEW",header=headers))
+        self.assertEquals(endpoint_response.status_code, 405)
+    
+    def test_course_endpoint_wrong_and_no_authorization(self):
+        authen = 'Bearer eyJhbGcwrongoneIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNoYWluIn0.Zrm7f9vw9qLA8dWxK9TRDZxO7U1XZQqSsgxFitf8Leg'
+        headers = {"Content-Type":"application/json","Authorization":str(authen)}
+        # wrong authen
+        endpoint_response = course_endpoint(courseTestCase.mock_request(self,"GET",header=headers))
+        self.assertEquals(endpoint_response.status_code, 401)
+        # no authen
+        endpoint_res = course_endpoint(courseTestCase.mock_request(self,"GET"))
+        self.assertEquals(endpoint_res.status_code, 401)
+        
--- backend/course/course/main_knowledge/tests.py
+++ backend/course/course/main_knowledge/tests.py
@@ -1,3 +1,97 @@
 from django.test import TestCase
+from main_knowledge.models import Knowledge
+from django.http import HttpRequest
+from main_knowledge.views import create_knowledge, knowledge_endpoint, knowledge_id_endpoint, search_knowledge
+from datetime import datetime
+import json
+from io import BytesIO
+from main_knowledge.serializers import KnowledgeSerializer
 
-# Create your tests here.
+class knowledgeTestCase(TestCase):
+
+    def setUp(self):
+        self.knowledge = Knowledge.objects.create(
+            id = "2",
+            author = "author007",
+            subject = "learn to fly",
+            content = "flyflyflyflyflyflyflyfly",
+            createDate = datetime(2020, 1, 12)
+        )
+
+    def mock_request(self, method, content = json.dumps({}), header = {"Content-Type":"application/json"}):
+        request1 = HttpRequest()
+        request1.method = method
+        body_content = content
+        request1._stream = BytesIO(body_content.encode())
+        request1.content_type = "application/json"
+        request1.headers = header
+
+        return request1
+
+    def get_token(self):
+        return "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNoYWluIn0.Zrm7f9vw9qLA8dWxK9TRDZxO7U1XZQqSsgxFitf8Leg"
+    # ======= end init =======
+
+    def test_model(self):
+        knowledge = Knowledge.objects.get(id="2")
+        self.assertEquals(knowledge.subject, "learn to fly")
+
+    def test_knowledge_endpoint_token_not_verify(self):
+        authen = self.get_token()
+        headers = {"Content-Type":"application/json","Authorization":str(authen+"123121564")}
+        # wrong token
+        endpoint_response = knowledge_endpoint(knowledgeTestCase.mock_request(self,"POST",header=headers))
+        self.assertEquals(endpoint_response.status_code, 401)
+        # no token
+        endpoint_res = knowledge_endpoint(knowledgeTestCase.mock_request(self,"POST"))
+        self.assertEquals(endpoint_res.status_code, 401)
+
+    def test_knowledge_endpoint_method_wrong(self):
+        authen = self.get_token()
+        headers = {"Content-Type":"application/json","Authorization":str(authen)}
+        endpoint_response = knowledge_endpoint(knowledgeTestCase.mock_request(self,"VIEW",header=headers))
+        self.assertEquals(endpoint_response.status_code, 405)
+
+    def test_knowledge_endpoint_POST_pass(self):
+        authen = self.get_token()
+        headers = {"Content-Type":"application/json","Authorization":str(authen)}
+        contents = json.dumps({"subject":"testSubject","content":"testContent"})
+        endpoint_response = knowledge_endpoint(knowledgeTestCase.mock_request(self,"POST",header=headers,content=contents))
+        self.assertEquals(endpoint_response.status_code, 201)
+    
+    def test_knowledge_endpoint_POST_fail(self):
+        authen = self.get_token()
+        headers = {"Content-Type":"application/json","Authorization":str(authen)}
+        contents = json.dumps({"subject":"testSubjectfail"})
+        endpoint_response = knowledge_endpoint(knowledgeTestCase.mock_request(self,"POST",header=headers,content=contents))
+        self.assertEquals(endpoint_response.status_code, 400)
+
+    def test_knowledge_endpoint_POST_no_username(self):
+        authen = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyYWRuYW1lIjoiZGVsMSJ9.STuyNkPLdj0uFV7SUtr_L9ggLJ0wcs39KKHu5LtB-Ro"
+        headers = {"Content-Type":"application/json","Authorization":str(authen)}
+        contents = json.dumps({"subject":"testSubjectfail","content":"testContent"})
+        endpoint_response = knowledge_endpoint(knowledgeTestCase.mock_request(self,"POST",header=headers,content=contents))
+        self.assertEquals(endpoint_response.status_code, 401)
+
+    def test_knowledge_endpoint_GET_exist(self):
+        authen = self.get_token()
+        headers = {"Content-Type":"application/json","Authorization":str(authen)}
+        contents = json.dumps({"query":"fly"})
+        request = knowledgeTestCase.mock_request(self,"GET",header=headers,content=contents)
+        endpoint_response = knowledge_endpoint(request)
+        self.assertEquals(endpoint_response.status_code, 200)
+
+    def test_knowledge_endpoint_GET_not_exist(self):
+        authen = self.get_token()
+        headers = {"Content-Type":"application/json","Authorization":str(authen)}
+        contents = json.dumps({})
+        request = knowledgeTestCase.mock_request(self,"GET",header=headers,content=contents)
+        request.query = "ppp"
+        endpoint_response = knowledge_endpoint(request)
+        self.assertEquals(endpoint_response.status_code, 200)
+        query = request.GET.get('query', '')
+        print("query = ",query)
+        obj = Knowledge.objects.filter(subject__contains=query) 
+        print("obj = ",obj)
+        data = {'results': KnowledgeSerializer(obj, many=True).data} 
+        print("Data= ",data)
