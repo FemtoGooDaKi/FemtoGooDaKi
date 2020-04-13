cc987073e5a4083831927b5e4ab8ae9020ebd2ff
SSripilaipong <vappakarat@gmail.com>
2020-04-14 01:49:38 +0700

    change primary key to only username

--- backend/course/course/main_enrollment/models.py
+++ backend/course/course/main_enrollment/models.py
@@ -4,9 +4,6 @@ from main_course.models import Course
 
 # Create your models here.
 class Enrollment(models.Model):
-    username = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, primary_key=True)
     courses = models.ManyToManyField(Course)
     enrollDate = models.DateField()
-
-    class Meta:
-        unique_together = (('username', 'course'),)
