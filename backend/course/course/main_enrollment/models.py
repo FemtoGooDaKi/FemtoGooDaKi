9e3fa05c897a838f93901086e48bdd0a1796f833
SSripilaipong <vappakarat@gmail.com>
2020-04-14 01:57:17 +0700

    remove enrollDate

--- backend/course/course/main_enrollment/models.py
+++ backend/course/course/main_enrollment/models.py
@@ -6,4 +6,3 @@ from main_course.models import Course
 class Enrollment(models.Model):
     username = models.CharField(max_length=30, primary_key=True)
     courses = models.ManyToManyField(Course)
-    enrollDate = models.DateField()
