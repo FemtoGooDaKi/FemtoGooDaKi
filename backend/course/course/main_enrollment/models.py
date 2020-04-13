68e6dd843dee446699f50ba3289caed749a591a8
SSripilaipong <vappakarat@gmail.com>
2020-04-14 01:40:46 +0700

    fix typo

--- backend/course/course/main_enrollment/models.py
+++ backend/course/course/main_enrollment/models.py
@@ -5,7 +5,7 @@ from main_course.models import Course
 # Create your models here.
 class Enrollment(models.Model):
     username = models.CharField(max_length=30)
-    course = models.ManyToManyField(Course)
+    courses = models.ManyToManyField(Course)
     enrollDate = models.DateField()
 
     class Meta:
