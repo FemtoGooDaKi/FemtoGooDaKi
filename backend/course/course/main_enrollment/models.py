70cb3818f49319595f7222f0d5f755f59baa4e3e
SSripilaipong <vappakarat@gmail.com>
2020-04-14 01:36:58 +0700

    add Enrollment model

--- backend/course/course/main_enrollment/models.py
+++ backend/course/course/main_enrollment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from main_course.models import Course
+
 
 # Create your models here.
+class Enrollment(models.Model):
+    username = models.CharField(max_length=30)
+    course = models.ManyToManyField(Course)
+    enrollDate = models.DateField()
+
+    class Meta:
+        unique_together = (('username', 'course'),)
